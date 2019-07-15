9864880bbdfa11365a0c4a4316e4d0bfdb7e6d55
Dmtocky <t.dmitriy@rsit.ru>
2019-07-15 14:22:04 +0400

    dev
    Former-commit-id: b476a2b816d607ebadd3337e35f85a54354ca158
    Former-commit-id: 83915de8136f871ebc217b566b867dc8e1833310 [formerly 6f77c832d2fa8f2e4fc14c1dfd03ba9dc472ef16]
    Former-commit-id: 4173c49b2cc541fbc98f75f2dfb3942ee33bac59

--- workers/strings_worker.py
+++ workers/strings_worker.py
@@ -7,11 +7,12 @@ class StringsWorker():
     @staticmethod
     def create_strings_material(m=False):
         ''' set material '''
+
         mi = MaterialInfo.get_material_info('strings_material',True)
 
         bd1 = mi['nodes'].new("ShaderNodeBsdfDiffuse")
         bd1.location = [ 0 , 0]
-        bd1.inputs[0].default_value = c
+        bd1.inputs[0].default_value = (0, 0, 0, 1)
 
         om = mi['nodes'].new("ShaderNodeOutputMaterial")
         om.location = [200, 0]
