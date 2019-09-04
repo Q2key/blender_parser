a76f3cb8f20a4d27c2320a9daa880a4393059649
Dmtocky <t.dmitriy@rsit.ru>
2019-09-04 16:31:36 +0400

    Правки, логер
    Former-commit-id: db4cb46c5950a75d9583d79879d7d8dbd93153ac
    Former-commit-id: e6bbcd17bd82f5f8784da0043c9f071fcb5bd915 [formerly 259427805f84e648122dd0789bd04974cf40f577]
    Former-commit-id: a6f50e58cac6b1fd0da216aa1574ede38bb183d8

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
+from commands.init_command import InitCommand
+from commands.scan_store_command import ScanStoreCommand
+from commands.reset_command import ResetCommand
+from commands.render_command import RenderCommand
+from helpers.logger import Logger as Logger
+from helpers.arguments_helper import ArgumentsHelper
+from instance import Instance
 import os
 import sys
 
@@ -6,14 +13,6 @@ root = os.path.dirname(__file__)
 sys.path.append(root)
 sys.path.append(root)
 
-
-from instance import Instance
-from helpers.arguments_helper import ArgumentsHelper
-
-from commands.render_command import RenderCommand
-from commands.reset_command import ResetCommand
-from commands.scan_store_command import ScanStoreCommand
-from commands.init_command import InitCommand
 
 parser = ArgumentsHelper()
 
@@ -26,17 +25,17 @@ parser.add_argument("-d", "--debug", action="store_true")
 args = parser.parse_args()
 
 if __name__ == "__main__":
+    
     cmd_stack = list()
     ctx = Instance()
     if args.reset:
-        cmd_stack.append(ResetCommand(ctx,args))
+        cmd_stack.append(ResetCommand(ctx, args))
     if args.store:
-        cmd_stack.append(ScanStoreCommand(ctx,args))
-        cmd_stack.append(RenderCommand(ctx,args))
+        cmd_stack.append(ScanStoreCommand(ctx, args))
+        cmd_stack.append(RenderCommand(ctx, args))
     if args.static:
-        cmd_stack.append(InitCommand(ctx,args))
-        cmd_stack.append(RenderCommand(ctx,args))
+        cmd_stack.append(InitCommand(ctx, args))
+        cmd_stack.append(RenderCommand(ctx, args))
 
     for cmd in cmd_stack:
         cmd.run()
-
