986f9db3dea3db19714629d0004660eb8f9dfed8
tocky-home <dimatocky@gmail.com>
2020-12-08 17:17:47 +0400

    Вычистил лишнее
    Former-commit-id: 710c4242ca9293d057abccd74dc6422122675bc8
    Former-commit-id: 13470d7f194b6ded8a1e0354feaccce1c3a4e23c [formerly fdcb4529579f0b1e6a4ceba20bfe3c4cb2461363]
    Former-commit-id: fe61d8115d9d9f6c80f34c8f84dcf7238c096889

--- render.bat
+++ render.bat
@@ -1,2 +1,2 @@
 set model_arg=%1
-call C:/blender/blender scenes/main/__MAIN__.blend --background --python run.py -- -store -m=%model_arg%
+start C:/blender/blender scenes/main/__MAIN__.blend --background --python run.py -- -store -m=%model_arg%
