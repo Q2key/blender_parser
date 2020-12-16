5b65539a2520a8ff797b5c970d6c53bb5e0efc8d
tocky-home <dimatocky@gmail.com>
2020-12-16 20:24:00 +0400

    file reader
    Former-commit-id: ffcba1018a15cfc4bfc350185881cfa1d82a14bf
    Former-commit-id: 9b5ada41fb9f0230c0f8c02c19db67bbe1295fe2 [formerly 80c33dd69e441f3378af5cbeeb5380f701feaf11]
    Former-commit-id: d9d91d923647ce8e1237da56183c0217c4dee62f

--- helpers/file_helper.py
+++ helpers/file_helper.py
@@ -6,26 +6,46 @@ import time
 
 
 class FileHelper:
-    
-    def watch_start(self):
-        self.start = time.time()
 
-    def watch_stop(self):
-        self.stop = time.time()
+    def open_file(self, path):
+        with open(path, mode="w+") as f:
+            self.file = f
 
-    def print_diff(self):
-        value = self.stop - self.start
+    def close_file(self):
+        if self.file.closed == False:
+            self.file.close()
 
-        valueD = (((value/365)/24)/60)
-        Days = int(valueD)
+    def write_stats(self, path, entity):
+        data_path = str.format("{0}/.dat", path)
+        json_data = self.read_json(data_path)
+        json_data[entity] = 'ok'
+        self.write_json(data_path, json_data)
 
-        valueH = (valueD-Days)*365
-        Hours = int(valueH)
+    def check_entity(self, path, entity):
+        data_path = str.format("{0}/.dat", path)
+        json_data = self.read_json(data_path)
 
-        valueM = (valueH - Hours)*24
-        Minutes = int(valueM)
+        if entity in json_data:
+            return False
+        else:
+            return True
 
-        valueS = (valueM - Minutes)*60
-        Seconds = int(valueS)
+    def read_json(self, path):
+        try:
+            if os.path.exists(path) == False:
+                self.write_json(path, dict(), False)
 
-        print(str.format("Days: {0}; Hours {1}; Minutes {2}; Seconds {3}", Days, Hours, Minutes, Seconds))
+            with open(path, mode='r') as f:
+                data = f.read()
+                return json.loads(data)
+
+        except OSError:
+            print("Creation of the directory %s failed" % path)
+
+    def write_json(self, file, data, close=True):
+        self.file.write(json.dumps(data, indent=4))
+        if close:
+            self.close_file()
+
+    def read_dat_file(self, path):
+        return self.read_json(str.format("{0}/.dat", path))
