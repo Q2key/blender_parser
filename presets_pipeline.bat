68b90a36dfc93bfa2997fcc51ad18041dcb36de1
Dmtocky <t.dmitriy@rsit.ru>
2020-12-29 15:00:00 +0400

    правки
    Former-commit-id: e14c27d32fd7f7522700f20722a7999f6f04e883
    Former-commit-id: a8983ffb5476f91f42ac113519d1897ea360cc6f [formerly ee58542da1c9e1f0787c0cbe0ac202a0164bbd11]
    Former-commit-id: d5323e4c6e730ba0dbc08cea15bd9d36a009a280

--- presets_pipeline.bat
+++
@@ -1,7 +0,0 @@
-call C:/blender29/blender scenes/__DEV29__.blend --background --python run.py -- -store --model=presetCasual --version=presets
-call C:/blender29/blender scenes/__DEV29__.blend --background --python run.py -- -store --model=presetBusinessCasual --version=presets
-call C:/blender29/blender scenes/__DEV29__.blend --background --python run.py -- -store --model=presetTuxedo --version=presets
-call C:/blender29/blender scenes/__DEV29__.blend --background --python run.py -- -store --model=presetBusiness --version=presets
-call C:/blender29/blender scenes/__DEV29__.blend --background --python run.py -- -store --model=presetFormal --version=presets
-
-call optimize-images -ca renders/presets/
