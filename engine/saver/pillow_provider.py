6555640dad0eeea2a4d003c6dd62433265e018ac
Dmtocky <t.dmitriy@rsit.ru>
2021-01-12 18:49:05 +0400

    Правки
    Former-commit-id: 53fd039a931f23e1bd5b942340216b6664e0041a
    Former-commit-id: de115238cc22f47915601569d09b5af3c059e97a [formerly 95516969b8ebf1ced9f6cbed28da310863fd8fda]
    Former-commit-id: 035298f9fdfe0cdf2636d481f9c5232990a62149

--- engine/saver/pillow_provider.py
+++ engine/saver/pillow_provider.py
@@ -6,13 +6,13 @@ import shutil
 class PillowProvider:
 
 	@staticmethod
-	def save_image(src, out, sizes, fmt="PNG", optimize=True):
+	def save_image(src, out, sizes, fmt="PNG", optimize=True, img_quality=95):
 		cp_file = src + ".png"
 		cp_img = shutil.copy(src, cp_file)
 
 		img = Image.open(cp_img)
 		img = img.resize((sizes["x"], sizes["y"]), Image.ANTIALIAS)
-		img.save(out, fmt, optimize=optimize)
+		img.save(out, fmt, optimize=optimize, quality=sizes["q"])
 		img.close()
 
 		os.remove(cp_file)
@@ -29,7 +29,7 @@ class PillowProvider:
 
 		background = Image.new("RGB", png.size, (255, 255, 255))
 		background.paste(png, mask=png.split()[3])
-		background.save(out, 'JPEG', progressive=True, optimize=True, quality=95)
+		background.save(out, 'JPEG', progressive=True, optimize=True, quality=sizes["q"])
 		
 		png.close()
 		os.remove(cp_file)
