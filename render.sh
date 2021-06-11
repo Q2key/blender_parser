51a963c959a5544d870f98edcaa3768f5b317420
Dmtocky <t.dmitriy@rsit.ru>
2021-06-11 12:43:54 +0400

    fix linux script
    Former-commit-id: 777b32cd3d718482e7441886d8cdfa320585dd98
    Former-commit-id: 20510e02069586d9473cce2ddeba8ae4fbc7295b [formerly 0290937af1700b6247b8225ebe34280f76886437]
    Former-commit-id: a4ed6b9c4f2d2f093590c7d05cbecf3d06781a6a

--- render.sh
+++ render.sh
@@ -1,1 +1,1 @@
-/home/matnik/render_engine/blender/blender /home/matnik/render_engine/blender_parser/scenes/remark_prod.blend --background --python run.py -- -store $@
+/home/matnik/render_engine/blender/blender /home/matnik/render_engine/blender_parser/scenes/remark_prod.blend --background --python run.py -- -s $@
